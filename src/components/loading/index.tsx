dcc49a4841822b101e5e645d35c328aaddfe2639
Carlos Francisco Herrera Franco <carlosfranciscoherrerafranco@gmail.com>
2024-04-12 17:30:51 -0600

    add loading component

---
+++ src/components/loading/index.tsx
@@ -0,0 +1,11 @@
+const LoadingPage = () => {
+	return (
+		<div className='loading'>
+			<div id='first'></div>
+			<div id='second'></div>
+			<div id='third'></div>
+		</div>
+	)
+}
+
+export default LoadingPage
